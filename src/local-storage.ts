d207364a769403098df7632a5ca6ef07c2bb3b22
Rohan-ult <61697340+Rohan-ult@users.noreply.github.com>
2020-12-15 19:23:31 +0600

    Update local-storage.ts

--- src/local-storage.ts
+++ src/local-storage.ts
@@ -3,16 +3,15 @@ import { join } from "https://deno.land/std/path/mod.ts";
 
 export class LocalStorage<T> extends KeyValue<T> {
     constructor(private _path: string = join(Deno.cwd(), "local-storage.json")) {
-        super()
-    }
-    load() {
-        return Deno.readTextFile(this._path)
-            .then(text => this.data = JSON.parse(text))
-            .catch(err => {
-                if (err instanceof Deno.errors.NotFound)
-                    return this.save()
+        super();
+        try {
+            this.data = JSON.parse(Deno.readTextFileSync(this._path))
+        } catch (err) {
+            if (err instanceof Deno.errors.NotFound)
+                Deno.writeTextFileSync(this._path, JSON.stringify(this.data))
+            else 
                 throw err
-            })
+        }
     }
     save() {
         return Deno.writeTextFile(this._path, JSON.stringify(this.data))
